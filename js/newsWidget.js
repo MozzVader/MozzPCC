d0df202437fe387561e55e8a131f5e3f56ee47d4
Z User <z@container>
2026-05-25 19:39:18 +0000

    fix(news): sacar Pagina/12, Ambito y Perfil de Argentina

--- js/newsWidget.js
+++ js/newsWidget.js
@@ -22,10 +22,7 @@
       sources: [
         { id: 'tn',       name: 'TN',           url: 'https://tn.com.ar/rss.xml', builtin: true },
         { id: 'clarin',   name: 'Clarin',       url: 'https://www.clarin.com/rss/', builtin: true },
-        { id: 'perfil',   name: 'Perfil',       url: 'https://www.perfil.com/feed', builtin: true },
-        { id: 'pagina12', name: 'Pagina/12',    url: 'https://www.pagina12.com.ar/rss/', builtin: true },
         { id: 'cronista', name: 'El Cronista',  url: 'https://www.cronista.com.ar/rss/', builtin: true },
-        { id: 'ambito',   name: 'Ambito',       url: 'https://www.ambito.com/rss/', builtin: true },
         { id: 'minutouno', name: 'Minuto Uno',  url: 'https://www.minutouno.com/rss.xml', builtin: true },
       ],
     },
